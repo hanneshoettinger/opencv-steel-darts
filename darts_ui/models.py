86a14a48ee0a8fa5623c8ab4959af89d28516084
Will Benica <w.benica@gmail.com>
2018-03-18 08:45:15 +0100

    index should show changes to the dart cam
    - add DB entry per button push
    - change the button depending on if status is True / False
    - Show time of last update

--- darts_ui/models.py
+++ darts_ui/models.py
@@ -10,43 +10,14 @@ class DartsRecognitionStatus(models.Model):
 
     def __str__(self):
         return "{}, {}".format(self.is_running, self.since)
-        #
-        # if current.is_running:
-        #     return "Status: {} since {}".format(current.is_running, current.since)
-        # else:
-        #     return "{}".format(current.is_running)
 
-    # TODO: Create methods to start and stop the dart recognition
-    # def start_recognition(self):
-    #     current = self.objects.get(pk=1)
-    #     if not current.is_running:
-    #         try:
-    #             kickoff()
-    #             current.objects.get(pk=1).update(is_running=True)
-    #             current.objects.get(pk=1).update(running_since=datetime.now())
-    #             return "Recognition started at {}.".format(current.since)
-    #         except:
-    #             raise EnvironmentError("Failed to start darts recognition")
-    #     else:
-    #         return "Recognition running since {}.".format(current.since)
-    #
-    # def stop_recognition(self):
-    #     if self.is_running:
-    #         try:
-    #             # TODO: kickoff() needs a way to stop
-    #             self.objects.get(pk=1).update(is_running=False)
-    #             self.objects.get(pk=1).update(running_since=datetime.now())
-    #             return "Recognition stopped at {}".format(self.since)
-    #         except:
-    #             raise ProcessLookupError("No way to turn off darts recognition")
-    #     else:
-    #         return "Recognition turned off since {}.".format(self.since)
 
     def current_status(self):
-        return self.objects.last().is_running
+        return self.is_running
 
     def running_since(self):
-        return self.objects.last().since
+        return self.since
 
+    # TODO:This always returns true (should be replaced with the kickoff function)
     def start_recognition(self):
         return return_status()
--- darts_ui/views.py
+++ darts_ui/views.py
@@ -9,54 +9,18 @@ from datetime import datetime
 # Create your views here.
 
 
-# class IndexView(generic.ListView):
-#     template_name = 'darts_ui/index.html'
-#     context_object_name = "status"
-#
-#     def start_stop(self, request):
-#         if request.GET.get('start'):
-#             try:
-#                 started = return_status()
-#                 if started:
-#                     s = DartsRecognitionStatus.objects.last()
-#                     s.is_running = True
-#                     s.since = datetime.now()
-#             except:
-#                 pass
-#         else:
-#             try:
-#                 stopped = return_status()
-#                 if stopped:
-#                     s = DartsRecognitionStatus.objects.last()
-#                     s.is_running = True
-#                     s.since = datetime.now()
-#             except:
-#                 pass
-#
-#
-#     def get_queryset(self):
-#         """
-#         Display the current status of the darts recognition
-#         """
-#         current = DartsRecognitionStatus.objects.last()
-#         # return {"status": current, "since": since}
-#         return current
-#
-#
-#
-
-
-
 def index(request):
+    # print("Request: {}".format(request))
     status = DartsRecognitionStatus.objects.last()
     if request.method == 'POST':
         try:
-            print(return_status())
             if return_status():
-                new_status = DartsRecognitionStatus(is_running=(not status.current_status()),since=datetime.now())
+                new_status = DartsRecognitionStatus(is_running=(not status.current_status()), since=datetime.now())
                 new_status.save()
-                print("Trying to render new page")
-                return render(request, 'darts_ui/index.html', {"status": new_status.is_running, "since": new_status.since})
+                return render(request,
+                              'darts_ui/index.html',
+                              {"status": new_status.is_running, "since": new_status.since}
+                              )
         except:
             pass
 
